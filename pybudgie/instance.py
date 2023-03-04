44936ef91640f035917e9fc0f40812c729129114
Fin Jordan <bignoldjordanf@gmail.com>
2023-03-04 17:15:53 +0000

    documented pbinstance class

--- pybudgie/instance.py
+++ pybudgie/instance.py
@@ -15,6 +15,20 @@ class PBInstance:
             projects: List[PBProject] = None,
             voters: List[PBVoter] = None,
     ):
+        """
+        Constructs a PBInstance from optional metadata, a budget and a list of projects and voters.
+
+        Parameters:
+            - description (str): A description of the objective of the instance.
+            - country (str): The country in which the instance takes place.
+            - region (str): The region of said country in which in the instance takes place.
+            - district (str): The district of said region in which the instance takes place.
+            - category (str): The category or categories of the instance.
+            - budget (int): The maximum budget of the instance.
+            - projects (List[PBProject]): A list of PBProject objects which wrap the projects to be funded.
+            - voters (List[PBVoter]): A list of PBVoter objects which wrap the voters and their votes on the projects.
+        """
+
         # These attributes can and should be accessed
         # directly from the object.
         self.description = description
