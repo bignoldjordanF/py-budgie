fbd3a4705f7079278ef25e83099206cae6042e26
Fin Jordan <bignoldjordanf@gmail.com>
2023-03-04 15:59:45 +0000

    demo creates pbinstance

--- demo.py
+++ demo.py
@@ -1,9 +1,21 @@
 import pbreader
+import pybudgie
 
 
 def main():
     contents = pbreader.read_pb_file('resources/poland_warszawa_2019_ursynow.pb')
-    print(contents.metadata)
+    metadata = contents.metadata
+
+    instance = pybudgie.PBInstance(
+        description = metadata['description'],
+        country = metadata['country'],
+        region = metadata['unit'],
+        district = metadata['district'],
+        category = metadata['subunit'],
+        budget = metadata['budget']
+    )
+    
+    print(instance)
 
 
 if __name__ == '__main__':
