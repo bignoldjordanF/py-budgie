f5511191aa4daee6496d4f96aa5a4d2c794ae8e5
Fin Jordan <bignoldjordanf@gmail.com>
2023-03-05 20:42:23 +0000

    refactored pbwelfare into solver.py

---
+++ pybudgie/solver.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class PBAlgorithm(Enum):
+    GREEDY = 0
+    RATIO_GREEDY = 1
+    SIMULATED_ANNEALING = 2
+    GENETIC_ALGORITHM = 3
+    DYNAMIC_PROGRAMMING = 4
+    BRANCH_AND_BOUND = 5
+
+
+class PBWelfare(Enum):
+    UTILITARIAN = 0
+    EGALITARIAN = 1
+    NASH = 2
+
