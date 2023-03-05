2319f607c8fe81e52af5323b6b726bca142617a7
Fin Jordan <bignoldjordanf@gmail.com>
2023-03-05 20:25:20 +0000

    added documentation

--- examples.py
+++ examples.py
@@ -2,26 +2,35 @@ from pybudgie import PBInstance, PBProject, PBVoter
 
 
 def simple():
+    # Syntax: PBInstance(
+    #   Optional Parameters...
+    # )
     instance = PBInstance(
-        budget = 10_000
+        budget = 10_000,
+        # ...
     )
     
+    # Add Projects
     instance.projects.append(PBProject(id=1, cost=2_500))
     instance.projects.append(PBProject(id=2, cost=6_500))
     instance.projects.append(PBProject(id=3, cost=1_500))
     instance.projects.append(PBProject(id=4, cost=8_000))
 
+    # Add Voters & Votes
     instance.voters.append(PBVoter(id=1, votes={1: 1, 3: 1, 4: 1}))
     instance.voters.append(PBVoter(id=2, votes={2: 1, 3: 1}))
     instance.voters.append(PBVoter(id=3, votes={3: 1, 4: 1}))
     instance.voters.append(PBVoter(id=3, votes={1: 1}))
+
     return instance
 
 
 def generated():
-    from pybudgie.generator import generate_instance
+    from pybudgie.pbgenerator import generate_instance
 
-    # Syntax: generate_instance(optional_params...)
+    # Syntax: generate_instance(
+    #   Optional Parameters...
+    # )
     instance: PBInstance = generate_instance(
         min_budget=1_000_000,
         max_budget=10_000_000,
