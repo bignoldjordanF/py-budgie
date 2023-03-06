784de70fdf76c5e2d0c81bb7569c28f209483bfb
Fin Jordan <bignoldjordanf@gmail.com>
2023-03-06 03:35:29 +0000

    changed field name to utilities

--- pybudgie/voter.py
+++ pybudgie/voter.py
@@ -9,7 +9,7 @@ class PBVoter:
             sex: str = '',
             neighborhood: str = '',
             voting_method: str = '',
-            votes: Dict[str, int] = None
+            utilities: Dict[str, int] = None
     ) -> None:
         """
         Constructs a PBVoter object from an id, optional project information and a dictionary of votes over projects.
@@ -21,7 +21,7 @@ class PBVoter:
             - neighborhood (str): The optional neighborhood of the voter.
             - voting_method (str): The optional method of voting used by the voter.
             Otherwise assumed from the parent instance.
-            - votes (Dict[str, int]): A mapping from project id to cardinal utility
+            - utilities (Dict[str, int]): A mapping from project id to cardinal utility
             that this voter derives from each project. For example, in approval
             voting, we might have {'34': 1}, meaning this voter approves project 34.
         """
@@ -31,7 +31,7 @@ class PBVoter:
         self.sex = sex
         self.neighborhood = neighborhood
         self.voting_method = voting_method
-        self.votes = {} if not votes else votes
+        self.utilities = {} if not utilities else utilities
 
     def __repr__(self) -> str:
         return str(self.__dict__)
